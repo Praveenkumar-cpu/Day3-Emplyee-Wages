722969bcea50c74d9cd19640c7672384bae6480a
praveen <praveenpkbadiger@gmail.com>
2022-08-04 16:52:41 +0530

    adding employee present or absent file

--- bridgelab/EmployeeWages.java
+++ bridgelab/EmployeeWages.java
@@ -4,7 +4,13 @@ public class EmployeeWages
 {
     public static void main(String[] args)
     {
-        System.out.println("Hi Welcome to employee wages ");
+        int IS_FULL_TIME = 1;
+
+        double empcheck= Math.floor(Math.random() *10) % 2;
+        if (empcheck == IS_FULL_TIME)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is Absent");
 
     }
 }
