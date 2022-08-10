613cd30d3cdb09511bb53e5e7db786b0b4f56d6c
praveen <praveenpkbadiger@gmail.com>
2022-08-10 14:21:12 +0530

    Refactor Code to method and class Compute Employee Wage

--- bridgelab/EmployeeWages.java
+++ bridgelab/EmployeeWages.java
@@ -2,77 +2,40 @@ package bridgelab;
 
 import java.util.Random;
 
-public class EmployeeWages
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Welcome to employee wage computation. ");
+public class EmployeeWages {
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 2;
+    public static final int MAX_HOURS_IN_MONTH = 18;
 
-        Random rand = new Random();
+    public static int computeEmpwage() {
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
-        int empHrs=0;
-
-        int salary;
-
-        int present = 1;
-
-        int absent = 2;
-
-        int part_time_present = 3;
-
-        int wage_per_hr = 20;
-
-        int full_day_hr = 8;
-
-        int part_time_hr = 8;
-
-        int max_working_days = 20;
-
-        int max_working_hrs = 100;
-
-        int working_hrs=0;
-
-        int working_days=0;
-
-        while (working_hrs < max_working_hrs && working_days < max_working_days) {
-
-            int check = rand.nextInt(3) + 1;
-
-            if (check == present) {
-
-                System.out.println("Employee is present.");
-
-            } else if (check == part_time_present) {
-
-                System.out.println("Employee is present for part time.");
-
-            } else {
-
-                System.out.println("Employee is absent.");
-
+        while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
             }
-
-            switch (check) {
-
-                case 1 -> empHrs = 8;
-                case 2 -> empHrs = 0;
-                case 3 -> empHrs = 4;
-
-            }
-
-            int this_day = empHrs*wage_per_hr;
-
-            System.out.println(this_day);
-
-            working_hrs = working_hrs + empHrs;
-
-            working_days = working_days + 1;
-
+            totalEmpHrs += empHrs;
+            System.out.println("Day#:  " + totalWorkingDays + "Emp Hr: " + empHrs);
         }
+        int totalEmpwage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Total Emp wage: " + totalEmpwage);
+                return totalEmpwage;
+    }
 
-        salary = working_hrs * wage_per_hr;
+    public static void main(String[] args) {
 
-        System.out.println("salary of month is : " + salary);
+        computeEmpwage();
 
     }
 
