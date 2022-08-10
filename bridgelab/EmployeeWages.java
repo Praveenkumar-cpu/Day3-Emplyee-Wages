b7a164c542384c1a603d3353d5dba37f4ca12497
praveen <praveenpkbadiger@gmail.com>
2022-08-10 13:37:14 +0530

    Cal wages till a condition of day reached for a month

--- bridgelab/EmployeeWages.java
+++ bridgelab/EmployeeWages.java
@@ -10,15 +10,15 @@ public class EmployeeWages
 
         Random rand = new Random();
 
-        int empHrs;
+        int empHrs=0;
+
+        int salary;
 
         int present = 1;
 
         int absent = 2;
 
         int part_time_present = 3;
-
-        int check=rand.nextInt(3)+1;
 
         int wage_per_hr = 20;
 
@@ -28,30 +28,51 @@ public class EmployeeWages
 
         int max_working_days = 20;
 
-        int max_working_hrs = 20;
+        int max_working_hrs = 100;
 
-        if (check == present) {
+        int working_hrs=0;
 
-            System.out.println("Employee is present.");
+        int working_days=0;
+
+        while (working_hrs < max_working_hrs && working_days < max_working_days) {
+
+            int check = rand.nextInt(3) + 1;
+
+            if (check == present) {
+
+                System.out.println("Employee is present.");
+
+            } else if (check == part_time_present) {
+
+                System.out.println("Employee is present for part time.");
+
+            } else {
+
+                System.out.println("Employee is absent.");
+
+            }
+
+            switch (check) {
+
+                case 1 -> empHrs = 8;
+                case 2 -> empHrs = 0;
+                case 3 -> empHrs = 4;
+
+            }
+
+            int this_day = empHrs*wage_per_hr;
+
+            System.out.println(this_day);
+
+            working_hrs = working_hrs + empHrs;
+
+            working_days = working_days + 1;
 
         }
 
-        else if (check == part_time_present) {
+        salary = working_hrs * wage_per_hr;
 
-            System.out.println("Employee is present for part time.");
-        }
-
-        else {
-
-            System.out.println("Employee is absent.");
-
-        }
-
-        switch (check) {
-            case 1 -> empHrs = 8;
-            case 2 -> empHrs = 0;
-            case 3 -> empHrs = 4;
-        }
+        System.out.println("salary of month is : " + salary);
 
     }
 
